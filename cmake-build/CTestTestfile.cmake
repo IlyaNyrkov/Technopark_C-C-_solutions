7de684461e1efad72be9e2866c3c0cc3fbee5daf
Ilya Nyrkov <71572784+IlyaNyrkov@users.noreply.github.com>
2021-03-17 22:04:11 +0300

    Delete cmake-build directory

--- cmake-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilyanyrkov/Technopark_C-C-_solutions
-# Build directory: /home/ilyanyrkov/Technopark_C-C-_solutions/cmake-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
